b8973dea0837848415e02f3ed7119681656b2954
kouta0530 <16jk047@ymklab.net>
2021-08-12 22:25:07 +0900

    feat(django): djangoで書き直す

--- puzzlemaker/__init__.py
+++ puzzlemaker/__init__.py
@@ -1,8 +1,8 @@
+import puzzlemaker.views
 from flask import Flask
-from flask_cors import CORS
+#from flask_cors import CORS
 
 app = Flask(__name__)
 
-import puzzlemaker.views
 
-CORS(app, allow_headers="Content-Type")
+#CORS(app, allow_headers="Content-Type")
--- puzzlemaker/setting.py
+++ puzzlemaker/setting.py
@@ -1,12 +1,12 @@
 import os
 import json
 
-
+"""
 from dotenv import load_dotenv
 from os.path import join, dirname
 dotenv_path = join(dirname(__file__), '.env')
 load_dotenv(dotenv_path, encoding="utf-8_sig")
-
+"""
 
 key = os.getenv("CRYPT_KEY").encode()
 iv = os.environ["IV"].encode()
