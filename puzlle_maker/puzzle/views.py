067d36e46ed92a25a7ef504871ba5fdf4bbc251a
kouta0530 <16jk047@ymklab.net>
2021-08-13 22:43:07 +0900

    feat(views): トップページの処理を追加

--- puzlle_maker/puzzle/views.py
+++ puzlle_maker/puzzle/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
 # Create your views here.
+
+
+def index(request):
+    return render(request, './search_result.html')
+
+
+def get_puzzle_data(request, id):
+    return HttpResponse('get puzzle json array response')
