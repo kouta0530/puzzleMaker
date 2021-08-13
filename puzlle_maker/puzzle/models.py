7d5ab4eac71ec5061bad351570ce364614cdf45a
kouta0530 <16jk047@ymklab.net>
2021-08-13 22:42:24 +0900

    feat(models): Puzzleモデルの作成

--- puzlle_maker/puzzle/models.py
+++ puzlle_maker/puzzle/models.py
@@ -1,3 +1,13 @@
 from django.db import models
-
+from django.core.validators import MaxValueValidator, MinValueValidator
 # Create your models here.
+
+
+class Puzzle(models.Model):
+    title = models.CharField(max_length=200, null=False, blank=False)
+    size = models.IntegerField(
+        validators=[MinValueValidator(2), MaxValueValidator(10)])
+    created_at = models.DateTimeField()
+    update_at = models.DateTimeField()
+    picture_url = models.TextField(max_length=2000)
+    user_id = models.CharField(max_length=200)
