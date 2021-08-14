5f3fb997246a0a8b39791a4c60cbadaf63585095
kouta0530 <16jk047@ymklab.net>
2021-08-14 23:47:05 +0900

    feat(puzzle_maker): django_sassの追加

--- puzlle_maker/puzlle_maker/settings.py
+++ puzlle_maker/puzlle_maker/settings.py
@@ -34,6 +34,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'puzzle.apps.PuzzleConfig',
+    'django_sass',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -129,6 +130,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+    [
+        os.path.join(BASE_DIR, "static"),
+    ]
+)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
