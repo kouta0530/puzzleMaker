807385d94a83584c317eedf368c92418ca1784a4
kouta0530 <16jk047@ymklab.net>
2021-08-16 12:09:57 +0900

    feat(tests): userモデルの存在性テストを追加

--- puzlle_maker/puzzle/tests.py
+++ puzlle_maker/puzzle/tests.py
@@ -1,3 +1,14 @@
 from django.test import TestCase
-
+from django.utils import timezone
+from puzzle.models import Puzzle
 # Create your tests here.
+
+
+class PuzzleTest(TestCase):
+    def setUp(self):
+        self.puzzle = Puzzle.objects.create(
+            title="test puzzle", size=2, created_at=timezone.now(),
+            update_at=timezone.now(), picture_url="test.png", user_id="1abc")
+
+    def test_valid_user(self):
+        self.assertIs(Puzzle.objects.filter(pk=1).exists(), True)
