8d19892ee4963294057a8756a8523f51aaa8793c
kouta0530 <16jk047@ymklab.net>
2021-08-17 23:39:40 +0900

    feat(views): pk順に上から30個のパズルデータをtemplateに渡す

--- puzlle_maker/puzzle/views.py
+++ puzlle_maker/puzzle/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from puzzle.models import Puzzle
 # Create your views here.
 
 
 def index(request):
-    return render(request, './search_result.html')
+    puzzle = Puzzle.objects.filter(pk__range=(1, 30))
+    return render(request, './search_result.html', puzzle=puzzle)
 
 
 def get_puzzle_data(request, id):
