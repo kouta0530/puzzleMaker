c46d920e47647d32bee11df2a935fd9be2dd376f
kouta0530 <16jk047@ymklab.net>
2021-09-08 00:49:25 +0900

    feat(static): 画面最下部までスクロールした際にモックの要素を読み込んでみる

--- puzlle_maker/static/js/script.js
+++ puzlle_maker/static/js/script.js
@@ -1,3 +1,31 @@
-const container = document.getElementsByClassName("puzzle-container");
+const container = document.querySelector(".puzzle-container");
 let reading = false;
 let loadedContents = 0;
+
+console.log("hello world");
+
+window.addEventListener("scroll", (e) => {
+  if (!reading) {
+    const bodyHeight = document.body.clientHeight;
+    const windowHeight = window.innerHeight;
+    const bottomPoint = bodyHeight - windowHeight;
+    const currentPoint = window.pageYOffset;
+
+    if (bottomPoint <= currentPoint) {
+      reading = false;
+      const div = document.createElement("div");
+      div.classList.add("puzzle");
+      div.innerHTML = `
+        <div>           
+          <img src="static/img/no_image.png" alt={{p.title}} class="puzzle-picture" >
+        </div>
+        <div>
+          <img src="" alt="投稿者アイコン" class="user-icon">
+          <p>モックタイトル</p>
+        </div>
+      `;
+
+      container.appendChild(div);
+    }
+  }
+});
