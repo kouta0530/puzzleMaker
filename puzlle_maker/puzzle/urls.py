6589558aca61e0e97e795541fc3b6dadeb841149
kouta0530 <16jk047@ymklab.net>
2021-09-09 19:23:43 +0900

    fix(urls): 余分な/によって404が出てしまう状況を解消する

--- puzlle_maker/puzzle/urls.py
+++ puzlle_maker/puzzle/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('/puzzles/<int:id>', views.get_puzzle_data, name='get puzzle data')
+    path('puzzles/<int:id>', views.get_puzzle_data, name='get puzzle data')
 ]
