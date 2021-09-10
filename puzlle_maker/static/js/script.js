3cb8c6e4a6d33d020953cfa125d392011ab08713
kouta0530 <16jk047@ymklab.net>
2021-09-10 11:26:54 +0900

    feat(static): apiで取得した要素を表示する

--- puzlle_maker/static/js/script.js
+++ puzlle_maker/static/js/script.js
@@ -3,7 +3,22 @@ let reading = false;
 let loadedContents = 0;
 let id = 0;
 
-window.addEventListener("scroll", async (e) => {
+const showAdditionalArticle = (article) => {
+  const div = document.createElement("div");
+  div.classList.add("puzzle");
+  div.innerHTML = `
+        <div>           
+          <img src="${article.picture_url}" alt="${article.title}" class="puzzle-picture" >
+        </div>
+        <div>
+          <img src="" alt="投稿者アイコン" class="user-icon">
+          <h3>${article.title}</h3>
+        </div>
+      `;
+  container.appendChild(div);
+};
+
+window.addEventListener("scroll", (e) => {
   if (!reading) {
     const bodyHeight = document.body.clientHeight;
     const windowHeight = window.innerHeight;
@@ -11,23 +26,17 @@ window.addEventListener("scroll", async (e) => {
     const currentPoint = window.pageYOffset;
 
     if (bottomPoint <= currentPoint) {
-      reading = false;
-      const div = document.createElement("div");
-      div.classList.add("puzzle");
-      div.innerHTML = `
-        <div>           
-          <img src="static/img/no_image.png" alt={{p.title}} class="puzzle-picture" >
-        </div>
-        <div>
-          <img src="" alt="投稿者アイコン" class="user-icon">
-          <p>モックタイトル</p>
-        </div>
-      `;
-
-      container.appendChild(div);
       reading = true;
-      const res = await fetch(`http://localhost:8000/puzzles/${id}`);
-      res.json().then((d) => console.log(d));
+      fetch(`http://localhost:8000/puzzles/${id}`)
+        .then((res) => res.json())
+        .then((articles) => {
+          for (article of articles) {
+            showAdditionalArticle(article);
+          }
+        })
+        .catch((e) => {
+          alert("取得に失敗しました");
+        });
       reading = false;
     }
   }
