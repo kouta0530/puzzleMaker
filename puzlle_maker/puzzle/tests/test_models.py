0c86e3b2128d0bc4201097d1ef2e3c8baea777d0
kouta0530 <16jk047@ymklab.net>
2021-09-12 00:29:55 +0900

    feat(tests): タイトルが200文字を超える時、バリデーションエラーが発火するかテストする

--- puzlle_maker/puzzle/tests/test_models.py
+++ puzlle_maker/puzzle/tests/test_models.py
@@ -27,3 +27,19 @@ class PuzzleTest(TestCase):
         self.assertEquals(Puzzle.objects.all().count(), 1)
         self.assertEquals(exception.message_dict,
                           {'title': ['This field cannot be blank.']})
+
+    def test_title_over_length_prevent(self):
+        with self.assertRaises(ValidationError) as e:
+            long_title_puzzle = Puzzle(
+                title="a" * 201, size=2, created_at=timezone.now(),
+                update_at=timezone.now(), picture_url="test.png",
+                user_id="user_2"
+            )
+            long_title_puzzle.full_clean()
+
+        exception = e.exception
+        self.assertEquals(Puzzle.objects.all().count(), 1)
+        self.assertEquals(
+            exception.message_dict,
+            {'title':
+             ['Ensure this value has at most 200 characters (it has 201).']})
