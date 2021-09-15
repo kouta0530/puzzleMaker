a1a5a9890afafc3e4de7cad4337a478ea535afe2
kouta0530 <16jk047@ymklab.net>
2021-09-15 23:55:13 +0900

    feat(tests): 追加データ取得のurlとviewの関数が適切に対応するかテストする

--- puzlle_maker/puzzle/tests/test_urls.py
+++ puzlle_maker/puzzle/tests/test_urls.py
@@ -1,9 +1,13 @@
 from django.test import TestCase
 from django.urls import reverse, resolve
-from puzzle.views import index
+from puzzle.views import index, get_puzzle_data
 
 
 class UrlsTest(TestCase):
     def test_index_url(self):
         url = reverse('index')
         self.assertEqual(resolve(url).func, index)
+
+    def test_get_additional_puzzle_url(self):
+        url = reverse('get puzzle data', kwargs={'id': 1})
+        self.assertEqual(resolve(url).func, get_puzzle_data)
