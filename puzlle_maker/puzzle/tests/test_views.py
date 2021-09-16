a584bc83eb23c4e6b98c8e379fd0209b9796cee1
kouta0530 <16jk047@ymklab.net>
2021-09-16 11:21:07 +0900

    feat(tests): インデックスページ遷移時、昇順で30個のパズルデータが渡されているか検証する

--- puzlle_maker/puzzle/tests/test_views.py
+++ puzlle_maker/puzzle/tests/test_views.py
@@ -1,5 +1,5 @@
 from django.test import TestCase, Client
-from django.urls import reverse, resolve
+from django.urls import reverse
 from puzzle.models import Puzzle
 from django.utils import timezone
 
@@ -7,18 +7,21 @@ from django.utils import timezone
 class ViewsTest(TestCase):
     def setUp(self):
         self.client = Client()
-        self.puzzle = Puzzle.objects.create(
-            title="test",
-            size=2,
-            created_at=timezone.now(),
-            update_at=timezone.now(),
-            picture_url="test.png",
-            user_id="abdg3fh"
-        )
+        for i in range(60):
+            self.puzzle = Puzzle.objects.create(
+                title="test" + str(i),
+                size=2,
+                created_at=timezone.now(),
+                update_at=timezone.now(),
+                picture_url="test.png",
+                user_id="abdg3fh"
+            )
 
     def test_get_index_page(self):
         response = self.client.get('')
         self.assertEquals(response.status_code, 200)
+        compared_data = list(Puzzle.objects.filter(pk__range=(1, 30)).values())
+        self.assertEquals(response.context['puzzle'], compared_data)
 
     def test_get_additional_puzzle_data(self):
         url = reverse('get puzzle data', kwargs={'id': 0})
