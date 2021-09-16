a1a0f41cd056e648c9dffe9b7cd7520136b62a73
kouta0530 <16jk047@ymklab.net>
2021-09-16 10:50:02 +0900

    feat(tests): 追加のパズルデータのAPIにリクエストを投げた時、200のレスポンスが帰るかテスト

--- puzlle_maker/puzzle/tests/test_views.py
+++ puzlle_maker/puzzle/tests/test_views.py
@@ -1,10 +1,26 @@
 from django.test import TestCase, Client
+from django.urls import reverse, resolve
+from puzzle.models import Puzzle
+from django.utils import timezone
 
 
 class ViewsTest(TestCase):
     def setUp(self):
         self.client = Client()
+        self.puzzle = Puzzle.objects.create(
+            title="test",
+            size=2,
+            created_at=timezone.now(),
+            update_at=timezone.now(),
+            picture_url="test.png",
+            user_id="abdg3fh"
+        )
 
     def test_get_index_page(self):
         response = self.client.get('')
+        self.assertEquals(response.status_code, 200)
+
+    def test_get_additional_puzzle_data(self):
+        url = reverse('get puzzle data', kwargs={'id': 0})
+        response = self.client.get(url)
         self.assertEquals(response.status_code, 200)
