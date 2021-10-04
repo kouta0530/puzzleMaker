eb6a6ba54ffedaa479b3ac8cf4f8a9055c7b86fe
kouta0530 <16jk047@ymklab.net>
2021-10-04 23:57:26 +0900

    feat(tests): webDriverを使って一般サイトをgetすることができた

--- puzlle_maker/puzzle/tests/test_integration.py
+++ puzlle_maker/puzzle/tests/test_integration.py
@@ -1,14 +1,25 @@
 from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from selenium import webdriver
+from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 
 
 class IntegrationTest(StaticLiveServerTestCase):
-    @classmethod
-    def setUpClass(cls):
-        super().setUpClass()
-        cls.selenium = webdriver.Chrome()
+    def setUp(self):
+        options = webdriver.ChromeOptions()
+        options.add_argument('--no-sandbox')
+        options.add_argument("--headless")
+        options.add_argument("--disable-dev-shm-usage")
 
-    @classmethod
-    def tearDownClass(cls):
-        cls.selenium.quit()
-        super().tearDownClass()
+        self.selenium = webdriver.Remote(
+            command_executor='http://selenium:4444/wd/hub',
+            desired_capabilities=DesiredCapabilities.CHROME,
+            options=options
+        )
+
+    def tearDown(self):
+        self.selenium.quit()
+
+    def test_get_index_page(self):
+        self.selenium.get('http://127.0.0.1:8000')
+        e = self.selenium.find_element_by_tag_name('head')
+        print('test', e.text)
