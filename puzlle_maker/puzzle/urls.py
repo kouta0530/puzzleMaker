6120c0174805eb4460384dfe50075cec21f5ece6
kouta0530 <16jk047@ymklab.net>
2021-10-14 16:13:25 +0900

    feat(urls): 検索用のURLを追加

--- puzlle_maker/puzzle/urls.py
+++ puzlle_maker/puzzle/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('puzzles/<int:id>', views.get_puzzle_data, name='get puzzle data')
+    path('puzzles/<int:id>', views.get_puzzle_data, name='get puzzle data'),
+    path('puzzles/<str:search_words>',
+         views.search_puzzle_data, name='search puzzle data')
 ]
