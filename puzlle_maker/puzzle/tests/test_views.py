c463da06b30658bd96a2b84bce5acbf3629f8c0c
kouta0530 <16jk047@ymklab.net>
2021-10-19 11:09:49 +0900

    feat(tests): 空白ごとに単語を区切り、複数の単語を使って検索ができるかテストする

--- puzlle_maker/puzzle/tests/test_views.py
+++ puzlle_maker/puzzle/tests/test_views.py
@@ -38,8 +38,18 @@ class ViewsTest(TestCase):
         additional_puzzles = response.json()
         self.assertEquals(len(additional_puzzles), 2)
 
-    def test_search_for_words_equals_title_puzzle_data(self):
+    def test_search_for_word_equals_title_puzzle_data(self):
         response = self.client.get('/puzzles/?search_words=test')
         self.assertEquals(response.status_code, 200)
         self.assertEquals(response.context['puzzle'],
                           list(Puzzle.objects.all().values())[:62])
+
+    def test_search_by_multiple_words(self):
+        Puzzle.objects.create(title="cat",
+                              size=2,
+                              created_at=timezone.now(),
+                              update_at=timezone.now(),
+                              picture_url="test.png",
+                              user_id="abdg3fh")
+        response = self.client.get("/puzzles/?search_words=test1 cat")
+        self.assertEquals(len(response.context['puzzle']), 12)
