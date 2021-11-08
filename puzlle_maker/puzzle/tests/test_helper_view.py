13ef01b1a83c200d5cdf7cc21c54bd3ef42dfc52
kouta0530 <16jk047@ymklab.net>
2021-11-08 21:39:59 +0900

    feat(tests): create_indexが条件に応じて適切にインデックスが作成できるか検証のためテストコードを用意する

---
+++ puzlle_maker/puzzle/tests/test_helper_view.py
@@ -0,0 +1,10 @@
+from django.test import TestCase
+from puzzle.helper import create_index
+
+
+class HelperFunctionTest(TestCase):
+    def test_create_index_mod_zero(self):
+        self.assertEquals(create_index(30), [i+1 for i in range(1)])
+
+    def test_create_index(self):
+        self.assertEquals(create_index(31), [i+1 for i in range(2)])
