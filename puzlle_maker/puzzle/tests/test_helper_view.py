7a62484f208c63945995f656686b68aff8aff867
kouta0530 <16jk047@ymklab.net>
2021-11-13 00:45:21 +0900

    fix(tests): helper関数修正に伴いテストコードを修正する

--- puzlle_maker/puzzle/tests/test_helper_view.py
+++ puzlle_maker/puzzle/tests/test_helper_view.py
@@ -4,7 +4,7 @@ from puzzle.helper import create_index
 
 class HelperFunctionTest(TestCase):
     def test_create_index_mod_zero(self):
-        self.assertEquals(create_index(30), [i+1 for i in range(1)])
+        self.assertEquals(create_index(30, 30), [i+1 for i in range(1)])
 
     def test_create_index(self):
-        self.assertEquals(create_index(31), [i+1 for i in range(2)])
+        self.assertEquals(create_index(31, 30), [i+1 for i in range(2)])
