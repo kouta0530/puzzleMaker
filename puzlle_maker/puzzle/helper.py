d3574df39ea4211c239a8618d91078dab67232b9
kouta0530 <16jk047@ymklab.net>
2021-11-13 00:43:39 +0900

    fix(helper): 分割する数を引数で指定できるように修正する

--- puzlle_maker/puzzle/helper.py
+++ puzlle_maker/puzzle/helper.py
@@ -1,4 +1,4 @@
-def create_index(puzzles_nums):
-    index = puzzles_nums / 30 if (puzzles_nums % 30) == 0\
-        else puzzles_nums / 30 + 1
+def create_index(puzzles_nums, margin):
+    index = puzzles_nums / margin if (puzzles_nums % margin) == 0\
+        else (puzzles_nums / margin) + 1
     return [i+1 for i in range(int(index))]
