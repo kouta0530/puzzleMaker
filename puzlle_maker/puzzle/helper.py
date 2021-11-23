210516a7f9fae2f1ac361f12ed100da9cc616e19
kouta0530 <16jk047@ymklab.net>
2021-11-23 23:55:28 +0900

    fix(helper): 個数が分割数より少ない場合はインデックスを１で返すように修正する

--- puzlle_maker/puzzle/helper.py
+++ puzlle_maker/puzzle/helper.py
@@ -1,4 +1,10 @@
 def create_index(puzzles_nums, margin):
-    index = puzzles_nums / margin if (puzzles_nums % margin) == 0\
-        else (puzzles_nums / margin) + 1
+    index = 1
+    if puzzles_nums / margin == 0:
+        index = 1
+    elif puzzles_nums % margin == 0:
+        index = puzzles_nums / margin
+    else:
+        index = (puzzles_nums / margin) + 1
+
     return [i+1 for i in range(int(index))]
