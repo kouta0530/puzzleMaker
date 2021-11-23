c4ab4d5773e573555b07f1a54a3e251a9990b457
kouta0530 <16jk047@ymklab.net>
2021-11-23 23:57:38 +0900

    fix(views): 共通の操作事項はヘルパー関数で行う

--- puzlle_maker/puzzle/views.py
+++ puzlle_maker/puzzle/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.db.models import Q
 from puzzle.models import Puzzle
-from puzzle.helper import create_index
+from puzzle.helper import create_index, more_than_specified_number
 import json
 # Create your views here.
 
@@ -15,7 +15,7 @@ def index(request):
 
 def get_puzzle_data(request, id):
     count = Puzzle.objects.all().count()
-    end = (id + 1) * 30 if (id + 1) * 30 <= count else count
+    end = more_than_specified_number(count, (id + 1) * 30)
     data = list(Puzzle.objects.filter().values())[id * 30:end]
     return HttpResponse(json.dumps(data, ensure_ascii=False, default=str),
                         content_type="application/json")
@@ -23,7 +23,7 @@ def get_puzzle_data(request, id):
 
 def search_puzzle_data(request):
     search_words = request.GET.get('search_words')
-    id = int(request.GET.get('id'))
+    id = int(request.GET.get('id')) if request.GET.get('id') else 1
 
     if search_words:
         search_words_list = search_words.split()
@@ -34,13 +34,16 @@ def search_puzzle_data(request):
             query |= q
 
         puzzle_model = Puzzle.objects.filter(query)
+        count = create_index(puzzle_model.count(), 30)
+        next_or_end_works_index = more_than_specified_number(
+            puzzle_model.count(), id*30)
 
         puzzle_data = {
-            'puzzle': list(puzzle_model[(id - 1) * 30:id * 30].values()),
+            'puzzle':
+            list(puzzle_model[(id - 1) * 30:next_or_end_works_index].values()),
             'search_words': search_words,
-            'count': create_index(len(puzzle_model), 2)
+            'count': count
         }
-
         return render(request, './search_result.html', puzzle_data)
     else:
         return redirect('index')
