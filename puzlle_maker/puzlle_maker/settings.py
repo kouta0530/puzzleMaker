0f541ef15e1e8bb2ce0b9b1f803bf99820e88bb3
kouta0530 <16jk047@ymklab.net>
2021-12-12 20:39:13 +0900

    fix(setting): import errorにならないように修正する

--- puzlle_maker/puzlle_maker/settings.py
+++ puzlle_maker/puzlle_maker/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
-from . import settings_local
 import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -22,8 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ['SECRET_KEY'] if os.environ.get('SECRET_KEY') \
-    else settings_local.SECRET_KEY
+if os.environ.get('SECRET_KEY'):
+    SECRET_KEY = os.environ['SECRET_KEY']
+else:
+    from . import settings_local
+    SECRET_KEY = settings_local.SECRET_KEY
+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
